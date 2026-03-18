0d9a65b8ef8a3bba3087284dcc14848faf611cb8
Martin Rouy <martinrouy03@gmail.com>
2026-03-18 22:57:04 +0100

    35 Buddhas new display

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ function App() {
   const tibetanFontSize = useAppSelector((state) => state.ui.tibetanFontSize);
 
   return (
-    <div className="app" style={{ '--tibetan-font-size': `${tibetanFontSize}px`, '--ui-font-size': `${tibetanFontSize / 20}rem` } as React.CSSProperties}>
+    <div className="app">
       <Header />
-      <main>
+      <main style={{ '--tibetan-font-size': `${tibetanFontSize}px`, '--ui-font-size': `${tibetanFontSize / 20}rem` } as React.CSSProperties}>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/text/:textId" element={<TextPage />} />
