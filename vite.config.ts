52c3b56638799a21f7c0b1c38267238d041f3898
Martin Rouy <martinrouy03@gmail.com>
2026-05-27 12:41:20 +0200

    cache Google Fonts offline via workbox runtimeCaching

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,29 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       workbox: {
-        globPatterns: ['**/*.{js,css,html,png,jpg,mp3,woff,woff2}'],
+        globPatterns: ['**/*.{js,css,html,png,jpg,mp3,woff,woff2,webp}'],
         maximumFileSizeToCacheInBytes: 30 * 1024 * 1024,
         skipWaiting: true,
         clientsClaim: true,
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-stylesheets',
+              expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-webfonts',
+              cacheableResponse: { statuses: [0, 200] },
+              expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 },
+            },
+          },
+        ],
       },
       manifest: {
         name: 'Mot à Mot',
